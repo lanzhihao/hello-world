a7e3b66c12e37ddafdfb589af2ea0ee73a4780b4
lanzh <276810003@qq.com>
2019-07-31 20:03:40 +0800

    server and client

--- tcp_client.c
+++ tcp_client.c
@@ -2,48 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 
-#define PORT 8888
+void error_handling(char *message);
 
-int main()
+int main(int argc, char *argv[])
 {
-	/*1 创建socket*/
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd == -1)
-	{
-	perror("socket failed");
-	exit(-1);
-	}
-	/*2 准备通信地址*/
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	/*设置为服务器进程的端口号*/
-	addr.sin_port = htons(PORT);
-	/*服务器所在主机IP地址*/
-	inet_aton("192.168.182.10", &addr.sin_addr);
+	int sock;
+	struct sockaddr_in serv_addr;
+	char messagep[30];
+	int str_len;
 
-	/*3 连接服务器*/
-	int res = connect(sockfd, 
-	(struct sockaddr *)&addr,
-	sizeof(addr));
-	if(res == -1)
+	if(argc != 3)
 	{
-	perror("connect failed");
-	exit(-1);
+		printf("Usage: %s <IP> <port>\n", argv[0]);
+		exit(1);			
 	}
-	printf("连接服务器成功....\n");
-	/*4 和服务器交换数据*/
-	char buf[100] = {0};
-	char *str = "借点钱可以吗...";
-	write(sockfd, str, strlen(str)) ;
-	read(sockfd, buf, sizeof(buf));
-	printf("服务器说:%s\n", buf);
 
-	/*关闭连接*/
-	close(sockfd);
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock == -1)
+		error_handling("socket() error");
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+	
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("connect() error");
+	
+	str_len = read(sock, message, sizeof(message) - 1);
+	if(str_len == -1)
+		error_handling("read() error");
+	
+	printf("Message from server: %s\n", message);
+	close(sock);
 	return 0;
+} 
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
