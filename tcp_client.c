c99d352376ef01c613baba26bf9e8d295c8feada
lanzh <276810003@qq.com>
2019-08-01 16:14:52 +0800

    client_huisheng_readLen

--- tcp_client.c
+++ tcp_client.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int sock;
 	struct sockaddr_in serv_addr;
 	char messagep[BUF_SIZE];
-	// int str_len;
+	int str_len, recv_len, recv_cnt;
 	int idx = 0, read_len = 0;
 
 	if(argc != 3)
@@ -47,10 +47,17 @@ int main(int argc, char *argv[])
 			break;
 
 		write(sock, message, strlen(message));
-		str_len = read(sock, message, BUF_SIZE -1);
+
+		recv_len = 0;
+		while(recv_len < str_len)
+		{
+			recv_cnt = read(sock, &message[recv_len], BUF_SIZE - 1);
+			if(recv_cnt == -1)
+				error_handling("read() error");
+			recv_len += recv_cnt;
+		}
 		message[str_len] = 0;
 		printf("Message from server: %s\n", message);
-
 	}	
 	close(sock);
 	return 0;
