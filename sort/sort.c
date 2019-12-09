e4b137003f6f44e88efeaaeadcfb4b264cf69a95
lanzhihao <276810003@qq.com>
2019-12-09 23:39:36 +0800

    xi er

--- sort/sort.c
+++ sort/sort.c
@@ -8,6 +8,19 @@ void swap(int *a, int *b)
   *b = temp;
 }
 
+void shell_sort(int arr[], int len)
+{
+	int gap, i, j;
+	int temp;
+	for(gap = len >> 1; gap > 0; gap = gap >> 1)
+	{
+		temp = arr[i];
+		for(j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+			arr[j + gap] = arr[j];
+		arr[j + gap] = temp;
+	}
+}
+
 void selection_sort(int arr[], int len)
 {
   int i, j;
